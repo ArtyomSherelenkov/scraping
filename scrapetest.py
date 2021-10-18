c660c396723c2e2df5d90031a300d9ee31b5ba2c
ArtyomSherelenkov <artem-venom-venom@yandex.ru>
2021-10-18 17:27:10 +0300

    Update scrapetest.py

--- scrapetest.py
+++ scrapetest.py
@@ -2,5 +2,5 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 html = urlopen('http://www.pythonscraping.com/pages/page1.html')
-bs = BeautifulSoup(html.read(), 'lxml')
+bs = BeautifulSoup(html.read(), 'html5lib')
 print(bs.h1)
